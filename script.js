739d981fe44360406baeb8af7207b5075268b568
Dung Minh <dungtuthon@gmail.com>
2026-04-18 04:21:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -170,3 +170,14 @@ setInterval(() => {
 function checkout() {
   alert("✅ Đặt hàng thành công! Shop sẽ liên hệ bạn.");
 }
+let time = 600; // 10 phút
+
+setInterval(() => {
+  let m = Math.floor(time / 60);
+  let s = time % 60;
+
+  document.getElementById("time").innerText =
+    m + ":" + (s < 10 ? "0" + s : s);
+
+  if (time > 0) time--;
+}, 1000);

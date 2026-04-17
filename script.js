2ce7b120ba67cd171cf8c74aedbfca2d5ef06253
Dung Minh <dungtuthon@gmail.com>
2026-04-18 04:18:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -158,7 +158,14 @@ setInterval(() => {
 
   setTimeout(() => noti.remove(), 3000);
 }, 5000);
+let slides = document.querySelectorAll(".slide");
+let index = 0;
 
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 3000);
 // CHECKOUT
 function checkout() {
   alert("✅ Đặt hàng thành công! Shop sẽ liên hệ bạn.");

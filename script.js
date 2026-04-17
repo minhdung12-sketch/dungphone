bb6b0de4317cffb1453799788642c9a6e04c079b
Dung Minh <dungtuthon@gmail.com>
2026-04-18 04:33:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -205,3 +205,16 @@ function removeItem(i) {
 }
 
 renderCart();
+renderCart();
+
+let time = 600;
+
+setInterval(() => {
+  let m = Math.floor(time / 60);
+  let s = time % 60;
+
+  document.getElementById("time").innerText =
+    m + ":" + (s < 10 ? "0" + s : s);
+
+  if (time > 0) time--;
+}, 1000);

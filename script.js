d0a6a70c72b965e4e5f4afad894ed8c1140937a3
minhdung12-sketch <dungtuthon@gmail.com>
2026-04-18 02:28:04 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function buy(name) {
-  alert("Bạn đã chọn mua: " + name);
+let count = 0;
+let total = 0;
+
+function add(name, price) {
+  count++;
+  total += price;
+
+  document.getElementById("count").innerText = count;
+  document.getElementById("total").innerText = total;
+
+  alert("Đã thêm: " + name);
 }
